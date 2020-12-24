a925c36cb4e8ca0456f2e8b0caa808ee085fcb18
z-sohaib <js_zouambia@esi.dz>
2020-12-24 09:52:04 +0100

    firebase and cloud firestore setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import firebase from 'firebase';
+
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCJ1bsT9am2Xg-3t2LzWIc55xXvB8SNcpY",
+  authDomain: "realtime-chat-vue-de598.firebaseapp.com",
+  projectId: "realtime-chat-vue-de598",
+  storageBucket: "realtime-chat-vue-de598.appspot.com",
+  messagingSenderId: "1010831033634",
+  appId: "1:1010831033634:web:4d550ea1076ff268c5917b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = admin.firestore();
+
+window.db = db;
+
+db.settings({
+  timestampsInSnapshots: true
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
